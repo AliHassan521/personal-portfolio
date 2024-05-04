5a2c9feaab0e87d651fc0692158a634d21d10ac0
Ali Hassan <alihassan057689234@gmail.com>
2024-05-04 11:53:32 -0700

    created sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Navbar from "./Components/navbar/Navbar";
+import "./app.scss";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section>
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section id="Portfolio">Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
